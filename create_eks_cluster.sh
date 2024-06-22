007f29ecbe68b84cb44c73a3bb5126af844c842a
celizondom <ceem3@msn.com>
2024-06-22 14:36:39 -0600

    Create create_eks_cluster.sh

---
+++ create_eks_cluster.sh
@@ -0,0 +1,1 @@
+eksctl create cluster --name eks-integrator-project-me --region us-east-1 --node-type t2.small --nodes 2 --with-oidc --ssh-access --ssh-public-key pin --managed --full-ecr-access --zones us-east-1a,us-east-1b,us-east-1c
